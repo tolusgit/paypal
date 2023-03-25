cf2aea5b8be0873cda7dc7aa1e9fd7e04c200e37
Cloud User <ec2-user@ip-172-31-90-150.ec2.internal>
2023-03-25 20:21:07 +0000

    new

---
+++ deploy.py
@@ -0,0 +1,5 @@
+echo "Welcome to Paypal"                 
+echo "Paypal your payment gateway"        
+echo "Paypal is the gateway of the future" 
+echo "Paypal is offering 30% discount"    
+echo "Good deals for returing clients"
